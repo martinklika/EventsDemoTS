a149a28eba645e9d4b2da8e07ba76547116b5483
Martin Klika <martink@watson.local>
2016-02-10 10:22:09 +0000

    Removed js files in app, added gitignore

--- app/event-list/event-list.module.js
+++
@@ -1,8 +0,0 @@
-var app;
-(function (app) {
-    var eventList;
-    (function (eventList) {
-        'use strict';
-        angular.module('app.eventList', ['ngRoute', 'ngResource']);
-    })(eventList = app.eventList || (app.eventList = {}));
-})(app || (app = {}));
--- app/product-list/product-list.module.js
+++
@@ -1,8 +0,0 @@
-var app;
-(function (app) {
-    var productList;
-    (function (productList) {
-        'use strict';
-        angular.module('app.productList', []);
-    })(productList = app.productList || (app.productList = {}));
-})(app || (app = {}));
